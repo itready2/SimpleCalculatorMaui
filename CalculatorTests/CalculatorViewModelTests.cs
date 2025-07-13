a90681bce643f161f3ea1d5f6661e32c921faf8e
itready2 <74393006+itready2@users.noreply.github.com>
2025-07-13 22:00:58 +0700

    Update Comment

--- CalculatorTests/CalculatorViewModelTests.cs
+++ CalculatorTests/CalculatorViewModelTests.cs
@@ -7,33 +7,23 @@ namespace CalculatorTests
     public class CalculatorViewModelTests
     {
         [TestMethod]
-        public void AddCommand_ValidInput_ReturnsCorrectResult()
+        public void AddCommand_ValidInput_ReturnsCorrectResult() //ทดสอบการทำงานของคำสั่งบวก (AddCommand) ว่าเมื่อกรอกเลขถูกต้อง ผลลัพธ์จะถูกต้องหรือไม่
         {
-            // Arrange
             var vm = new CalculatorViewModel { Number1 = "10", Number2 = "7" };
-
-            // Act
             vm.AddCommand.Execute(null);
-
-            // Assert
             Assert.AreEqual("17", vm.Result);
         }
 
         [TestMethod]
-        public void DivideCommand_DivideByZero_ReturnsErrorMessage()
+        public void DivideCommand_DivideByZero_ReturnsErrorMessage()//ทดสอบการหารด้วยศูนย์ ว่าจะแสดงข้อความ Error ที่ถูกต้องหรือไม่
         {
-            // Arrange
             var vm = new CalculatorViewModel { Number1 = "5", Number2 = "0" };
-
-            // Act
             vm.DivideCommand.Execute(null);
-
-            // Assert
             Assert.AreEqual("Cannot divide by zero", vm.Result);
         }
 
         [TestMethod]
-        public void MultiplyCommand_ValidInput_ReturnsCorrectResult()
+        public void MultiplyCommand_ValidInput_ReturnsCorrectResult()//ทดสอบการคูณเลข เมื่อกรอกเลขถูกต้อง ผลลัพธ์ต้องถูกต้อง
         {
             var vm = new CalculatorViewModel { Number1 = "4", Number2 = "5" };
             vm.MultiplyCommand.Execute(null);
@@ -41,7 +31,7 @@ namespace CalculatorTests
         }
 
         [TestMethod]
-        public void SubtractCommand_InvalidInput_ReturnsInvalidInput()
+        public void SubtractCommand_InvalidInput_ReturnsInvalidInput()//ทดสอบกรณีที่กรอกค่าผิดพลาด เช่น กรอกตัวอักษร แทนตัวเลข ต้องแสดงข้อความแจ้งเตือน
         {
             var vm = new CalculatorViewModel { Number1 = "abc", Number2 = "2" };
             vm.SubtractCommand.Execute(null);
